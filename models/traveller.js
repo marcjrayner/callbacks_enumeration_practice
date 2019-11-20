91b9591521456c735e12f0562365481ddd4781b9
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-20 16:18:51 +0000

    first few tests pass

--- models/traveller.js
+++ models/traveller.js
@@ -3,15 +3,22 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map( (journey) => {
+    return journey.startLocation
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map( (journey) => {
+    return journey.endLocation
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const result = this.journeys.filter ( (journey) => {
+    return transport === journey.transport
+  });
+  return result
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
